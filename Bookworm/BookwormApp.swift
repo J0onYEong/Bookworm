06a83ca53af2f17c66ddda9c0ca78c5a954a6313
choijun0 <cjy00050005@gmail.com>
2022-12-12 11:35:50 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by 최준영 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
